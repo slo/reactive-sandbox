4f7b401220e57e9bba3ec7a7e03662a4f5e569be
Slawomir Luty <slawomir.luty@gmail.com>
2023-08-31 00:19:35 +0200

    Make it purely nonblocking :D - netty requirement

--- recurrency-app/src/main/java/sl/testapp/recurrency/FibController.java
+++ recurrency-app/src/main/java/sl/testapp/recurrency/FibController.java
@@ -16,11 +16,11 @@ import reactor.core.publisher.Mono;
 public class FibController {
 
 	@GetMapping("/fib/{n}")
-	public String fibn(@PathVariable int n) {
+	public Mono<String> fibn(@PathVariable int n) {
 		log.debug("Received request for n=[" + n + "]");
 		if(n<2) {
 			log.debug("Returning response for n=[" + n + "]");
-			return String.valueOf(n);
+			return Mono.just(String.valueOf(n));
 		} else {
 			Mono<Integer> n_1 = WebClient.create("http://localhost:8081").get().uri("/fib/{n}", n-1)
 					.accept(MediaType.TEXT_HTML)
@@ -34,11 +34,8 @@ public class FibController {
 					.flatMap(resp -> resp.bodyToMono(String.class))
 					.map(Integer::parseInt);
 			
-			Integer a = n_1.block(Duration.ofSeconds(60));
-			Integer b = n_2.block(Duration.ofSeconds(60));
-			
-			log.debug("Returning response for n=[" + n + "]");
-			return String.valueOf(a+b); 
+			log.debug("Returning 'mono' for n=[" + n + "]");
+			return n_1.flatMap(a -> n_2.map(b -> a+b)).map(String::valueOf);
 		}
 	}
 }
